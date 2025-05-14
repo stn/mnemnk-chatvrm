d2de7bbad00652580c779be5a7833c86c8ad0cb5
Akira Ishino <akira@lumilab.jp>
2025-05-14 21:55:54 +0900

    transparent background

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,12 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body style={{ backgroundImage: `url(${buildUrl("/bg-c.png")})` }}>
+      <body
+        style={{
+          // backgroundImage: `url(${buildUrl("/bg-c.png")})`,
+          backgroundColor: "transparent",
+        }}
+      >
         <Main />
         <NextScript />
       </body>
